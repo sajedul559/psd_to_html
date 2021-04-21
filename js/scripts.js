10c68b4381340d7f64fd2fe5159ab0c4b6adb06d
sajedul559 <54328015+sajedul559@users.noreply.github.com>
2021-04-21 22:48:30 +0600

    psd to html convert

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function(){
+
+jQuery('.menu').click(function(){
+
+	jQuery('ul').toggleClass('active');
+});
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+});
